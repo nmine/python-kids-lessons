8f2f88c60cc6e8f308dd897373dd0d76c870d139
nicolasmiine <nicolas.mine11@gmail.com>
2020-01-23 22:39:50 +0100

    first commit

---
+++ first_lesson/doc/test.py
@@ -0,0 +1,44 @@
+## variable
+my_message = "Helo World"
+My_second_message = "Hello2"
+print(my_message)
+
+## True / False
+value = 1
+other_value = 2
+result = value < other_value
+print(result)
+
+##if else
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+## iteration
+while True:
+    print("test")
+
+i = 0
+while i < 5:
+    i+1
+    print("test")
+
+numbers = [10, 12, 15, 18, 20]
+for number in numbers:
+    print(number)
+
+## prompt avec variable
+prompt = 'What is your guess? '
+players_guess = input(prompt)
+print(players_guess)
+
+##functions
+def my_function():
+    print("Hello from a function")
+while True:
+    print("test")
+
+
+
+
